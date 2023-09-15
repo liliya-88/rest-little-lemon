004659f43dbc69b862000a3db820bb1019d4a8bb
master <666Lilly888@gmail.com>
2023-09-16 02:57:33 +0300

    some changes

--- src/components/reservations/Reservations.jsx
+++ src/components/reservations/Reservations.jsx
@@ -336,12 +336,12 @@ const Reservations = () => {
                     </option>
                     <option value='13:00'>13:00</option>
                     <option value='15:00'>15:00</option>
-                    <option value='17:30'>17:30</option>
+                    <option value='16:00'>22:00</option>
+                    <option value='17:00'>17:00</option>
                     <option value='18:00'>18:00</option>
                     <option value='19:00'>19:00</option>
                     <option value='20:00'>20:00</option>
                     <option value='21:00'>21:00</option>
-                    <option value='22:00'>22:00</option>
                   </select>
                 </label>
                 <span
